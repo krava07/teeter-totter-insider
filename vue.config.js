b43b1c0476b9ccfe1f4f36a4d65e3fa4725fea9e
Vlad Kravchenko <krava07@gmail.com>
2021-06-03 02:32:26 +0300

    deploy script

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
         prependData: '@import "@/assets/variables.scss";'
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/teeter-totter-insider/'
+    : '/'
 }
